eb6ce56eb872fa7bf41fa8b75eba6f8eb7cebe5c
modengann <99417989+modengann@users.noreply.github.com>
2022-09-22 10:16:12 -0400

    init

---
+++ src/suicide_fractions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from os import sep
+import pandas as pd
+
+def suicide_fractions():
+    pass
+
+def main():
+    suicide_fractions()
+
+if __name__ == "__main__":
+    main()
